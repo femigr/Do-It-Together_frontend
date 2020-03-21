f95f13a445721769659990ec9855e68dc5857c12
Samir <samir.wafa@alexanderthamm.com>
2020-03-21 11:55:42 +0100

    Created rudimentary create-activity component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateActivityComponent } from './create-activity/create-activity.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateActivityComponent
   ],
   imports: [
     BrowserModule,
---
+++ my-app/src/app/create-activity/create-activity.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-activity',
+  templateUrl: './create-activity.component.html',
+  styleUrls: ['./create-activity.component.scss']
+})
+export class CreateActivityComponent implements OnInit {
+
+  activities = ['Sport', 'Lesen'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
