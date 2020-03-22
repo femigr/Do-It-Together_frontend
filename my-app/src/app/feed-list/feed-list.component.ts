7a6d40f322007efa2bd59e256c241923a83cdd94
Samir <samir.wafa@alexanderthamm.com>
2020-03-22 16:28:49 +0100

    Fixed fetch from server

--- my-app/src/app/feed-list/feed-list.component.ts
+++ my-app/src/app/feed-list/feed-list.component.ts
@@ -1,28 +1,28 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Activity } from '../model/activity';
 
 @Component({
   selector: 'app-feed-list',
   templateUrl: './feed-list.component.html',
-  styleUrls: ['./feed-list.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./feed-list.component.scss']
 
 })
 export class FeedListComponent implements OnInit {
 
   actionsUrl = 'https://lvve8kdo9b.execute-api.eu-central-1.amazonaws.com/prod/actions';
-  @Input() actions = [];
+  actions: Activity[] = [];
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
 
-    this.http.get(this.actionsUrl).subscribe((data) => {
-      var items: any;
+    this.http.get(this.actionsUrl).subscribe(data => {
+      let items: any;
       items = data['data']['Items'];
       for (const item of items) {
-        this.actions.push(item.Action)
+        this.actions.push(item.Action);
       }
 
     });
